2827e080aaf8c85ae8cea4180a4849d124d1cbcb
João Vitor <jvitordiass@outlook.com>
2024-03-05 17:52:29 -0300

    chore: add new rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,12 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off'
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off'
   }
 }
